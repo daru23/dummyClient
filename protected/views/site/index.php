4f3c6fe19c530870826c6a51ea4f2a896770d02a
daru23 <daru015@gmail.com>
2013-05-12 13:06:07 -0430

    Primer commit

---
+++ protected/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<br />
+<br />
+<h1>Bienvenido a <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<br />
+<br />
+<p>Este es un cliente dummy para probar las funcionalidades de tuCuenta.De.</p>
+
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
---
+++ protected/views/site/pages/about.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Nosotros';
+$this->breadcrumbs=array(
+	'Nosotros',
+);
+?>
+
+
+<h1>Nosotros</h1>
+
+<br/>
+<br/>
+<p>Este es un cliente dummy para probar el funcionamiento del Sistema de Ingreso Unico, conocido como tuCuenta.De.</p>
